3c8e47e6d5c7b8a9a266ee3589cb9540af23f257
Eunice <eunice911qq@gmail.com>
2021-12-16 13:13:47 +0800

    完成pre1練習程式碼

---
+++ j1215-單位換算表格、suss/js/main.js
@@ -0,0 +1,13 @@
+alert("Good afternoon");
+
+let x,y;
+x=70;
+y=80;
+console.log(x+y);
+
+let mystring = "Hello JJJJJJS";
+console.log(mystring);
+let myStringLength = myString.length;
+console.log(mystringLength);
+
+
